ab9f9736b8c7654a0627de241ccfa4c74e544654
roloramros <roloramros@gmail.com>
2026-08-16 18:01:58 -0400

    Fix Compose weight import in navigation drawer

--- app/src/main/java/com/gmp/offline/ui/common/GmpNavigationDrawer.kt
+++ app/src/main/java/com/gmp/offline/ui/common/GmpNavigationDrawer.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Divider
